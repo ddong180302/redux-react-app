517857d67dfaa4c020599976839ced1f3818cfec
{ddong180302} <{trandangdong18032002@gmail.com}>
2023-03-08 00:55:47 +0700

    commit use state

--- src/App.js
+++ src/App.js
@@ -4,28 +4,44 @@ import React, { useState } from 'react';
 
 function App() {
 
-  let [name, setName] = useState('');
+  let [name, setName] = useState('dong');
   const [ten, setTen] = useState('');
+  const [todos, setTodos] = useState([
+    { id: '1', title: 'dong' },
+    { id: '2', title: 'thao' }
+  ])
 
   /*name: giá trị của biến
   setName: hàm xử lý khi biến có sự thay đổi
   */
-  const handleOnclick = () => {
-    console.log(".....state: ", name)
-    setName(ten);
+  const handleOnclick = (event) => {
+    if (!ten) {
+      alert('empty todo!')
+      return;
+    }
+    let newTodo = { id: '', title: ten }
+    setTodos([...todos, newTodo])
+    setTen('')
   }
 
-  const handleOnchaneInput = (event) => {
-    setTen(event.target.value);
+  const handleOnchange = (event) => {
+    setTen(event.target.value)
   }
   return (
     <div className="App">
       <Nav />
-      <div>Hello world with {name}</div>
-      <input type="text" value={ten} onChange={(event) => handleOnchaneInput(event)} />
-      <button type='button' onClick={() => handleOnclick()}>
-        click me
-      </button>
+      <h1>hello world with {name}</h1>
+      <div className='todo-container'>
+        <div className='todo-list'>
+          {todos.map(item => {
+            return (
+              <li className='todo-child' key={item.id}> {item.title}</li>
+            )
+          })}
+        </div>
+      </div>
+      <input type='input' value={ten} onChange={(event) => handleOnchange(event)} />
+      <button type='button' onClick={(event) => handleOnclick(event)}>click me</button>
     </div>
   );
 }
