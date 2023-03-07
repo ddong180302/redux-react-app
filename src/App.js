575092a552a8ff70f1d393690bb759eeb1d96b1a
{ddong180302} <{trandangdong18032002@gmail.com}>
2023-03-07 22:23:07 +0700

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import Nav from './views/Nav';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  let [name, setName] = useState('');
+  const [ten, setTen] = useState('');
+
+  /*name: giá trị của biến
+  setName: hàm xử lý khi biến có sự thay đổi
+  */
+  const handleOnclick = () => {
+    console.log(".....state: ", name)
+    setName(ten);
+  }
+
+  const handleOnchaneInput = (event) => {
+    setTen(event.target.value);
+  }
   return (
     <div className="App">
       <Nav />
+      <div>Hello world with {name}</div>
+      <input type="text" value={ten} onChange={(event) => handleOnchaneInput(event)} />
+      <button type='button' onClick={() => handleOnclick()}>
+        click me
+      </button>
     </div>
   );
 }
